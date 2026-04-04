3f6e79fc36138019cc6f2e01e328fdc8069c8269
weimaotian <maotianwei6@gmail.com>
2026-04-05 07:39:58 +0800

    Update stock_bot.py

--- src/stock_bot.py
+++ src/stock_bot.py
@@ -504,7 +504,7 @@ def checklist_button_click(call):
     bot.edit_message_text(
         chat_id=call.message.chat.id,
         message_id=call.message.message_id,
-        text="点击切换状态"
+        text="点击切换状态",
         reply_markup=types.InlineKeyboardMarkup(inline_keyboard),
     )
 
